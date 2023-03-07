116a118c2e03012212becb9ddd9c6b7235e265b6
Suprame4 <adriancortesgonz@gmail.com>
2023-03-07 11:37:09 -0800

    complete exercise 1 - creating routers

---
+++ routes/user.js
@@ -0,0 +1,34 @@
+//import packages 
+const express = require('express')
+const route = express.Router()
+
+// List of Users
+let users = [
+    {
+        name: "User 1",
+        age: 30
+    },
+    {
+        name: "User 2",
+        age: 45
+    },
+    {
+        name: "User 3",
+        age: 27
+    },
+    {
+        name: "User 4",
+        age: 22
+    }
+]
+
+route.get('/', (req, res) => {
+    res.json(users)
+})
+
+route.get('/:id', (req, res) => {
+    const user = users[req.params.id - 1]
+    res.json(user)
+})
+
+module.exports = route
--- server.js
+++ server.js
@@ -2,49 +2,14 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-// List of Users
-let users = [
-    {
-        name: "User 1",
-        age: 30
-    },
-    {
-        name: "User 2",
-        age: 45
-    },
-    {
-        name: "User 3",
-        age: 27
-    },
-    {
-        name: "User 4",
-        age: 22
-    }
-]
-
-// List of Fruits
-let fruits = [
-    {
-        name: "Apple",
-        color: "Red"
-    },
-    {
-        name: "Banana",
-        color: "Yellow"
-    },
-    {
-        name: "Kiwi",
-        color: "Green"
-    },
-    {
-        name: "Grape",
-        color: "Purple"
-    },
-]
+//import routers 
+const fruitRouter = require('./routes/fruit')
+const userRouter = require('./routes/user')
 
 // Express Routes
+app.use('/fruit', fruitRouter)
 
-
+app.use('/user', userRouter)
 
 
 app.listen(port, () => {
